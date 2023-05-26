c553f033b89c729767eaf06005f1eddcfdea5238
avanturer <72664467+avanturer@users.noreply.github.com>
2023-05-26 11:25:12 +0300

    Update regression.cpp

--- prj.lab/regression/regression.cpp
+++ prj.lab/regression/regression.cpp
@@ -1,12 +1,4 @@
 //
 // Created by hedge on 13.05.2023.
 //
-#include <iostream>
-#include <utility>
-#include <vector>
-#include <fstream>
-#include <sstream>
-#include <string>
-#include <algorithm>
-#include <cmath>
 #include "regression/regression.hpp"
